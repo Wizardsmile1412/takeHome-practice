dc91002d89a0683754bad00a56de8b97c5c85f18
Pawarit Sripayom <psripayom.s@wisdomvast.com>
2026-02-21 22:02:06 +0700

    feat(usePropertyFilters): filter and sort logic in usePropertyFilters hook

---
+++ rent-calculate/src/hooks/usePropertyFilters.ts
@@ -0,0 +1,48 @@
+import { useMemo, useState } from "react";
+import type { Property } from "../types/property";
+
+interface FilterState {
+    minPrice: number;
+    maxPrice: number;
+    bedrooms: number | null; // null = "All"
+    availableMonth: string | null; //null = "All", format: "2026-03"
+}
+
+type SortOption = 'price-asc' | 'price-desc' | 'date-asc' | 'date-desc';
+
+export function usePropertyFilters(properties: Property[]) {
+    const [filters, setFilters] = useState<FilterState>({
+        minPrice: 0,
+        maxPrice: 80000,
+        bedrooms: null,
+        availableMonth: null,
+    });
+    const [sortBy, setSortBy] = useState<SortOption>('price-asc');
+
+    const filteredProperties = useMemo(() => {
+        return properties
+        .filter(p=> p.price >= filters.minPrice && filters.maxPrice)
+        .filter(p => filters.bedrooms ? p.bedrooms === filters.bedrooms : true)
+        .filter(p => {
+            if (!filters.availableMonth) return true;
+            return p.availableDate.startsWith(filters.availableMonth)
+        })
+        .sort((a,b) => {
+            switch (sortBy) {
+                case 'price-asc': return a.price - b.price;
+                case 'price-desc': return b.price - a.price;
+                case 'date-asc': return new Date(a.availableDate).getTime() - new Date(b.availableDate).getTime();
+                case 'date-desc': return new Date(b.availableDate).getTime() - new Date(a.availableDate).getTime();
+                default: return 0;
+            }
+        });
+    }, [properties, filters, sortBy])
+
+    return {
+        filteredProperties,
+        filters,
+        setFilters,
+        sortBy,
+        setSortBy,
+    };
+}
