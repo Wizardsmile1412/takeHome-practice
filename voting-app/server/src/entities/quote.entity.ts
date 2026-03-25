23491cf02e2ea93b193bda1498469c512b559ebd
Pawarit (personal) <p.sripayom@gmail.com>
2026-03-25 11:57:50 +0700

    feat: wire TypeORM/SQLite and global ValidationPipe

---
+++ voting-app/server/src/entities/quote.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm'
+import { Vote } from './vote.entity'
+
+@Entity()
+export class Quote {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    text: string
+
+    @Column({ default: 0})
+    voteCount: number
+
+    @OneToMany(() => Vote, (vote) => vote.quote)
+    votes: Vote[]
+}
